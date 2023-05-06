5be8e86ff3686d6c17d62603910cbc12adb0211f
sitra0612 <88793669+sitra0612@users.noreply.github.com>
2023-05-06 22:33:32 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+function AddPersonForm() {
+  const [ person, setPerson ] = useState("");
+    
+   function handleChange(e) {
+    setPerson(e.target.value);
+  }
+    
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" 
+        placeholder="Add new contact" 
+        onChange={handleChange} 
+        value={person.name} />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+function PeopleList(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index) =>
+    <li key={index}>{val}</li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
+const el = (
+  <div>
+    <AddPersonForm />
+    <PeopleList data={contacts} />
+  </div>
+);
+ReactDOM.render(
+  el, 
+  document.getElementById('root')
+);
